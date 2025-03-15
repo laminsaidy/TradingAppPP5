761452e7b0b0e6a5375fe7c7f279c06f2c9b9d5c
LX <132520325+laminsaidy@users.noreply.github.com>
2025-03-16 00:25:00 +0100

    Add the conversation and dashboard urls to the project's main urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls', namespace='products')),  
+    path('dashboard/', include('dashboard.urls')),
+    path('conversation/', include('conversation.urls')),
 ]
