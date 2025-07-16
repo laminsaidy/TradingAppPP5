af93407c04d2fe489ac899f503838e97f81ebc66
LX <132520325+laminsaidy@users.noreply.github.com>
2025-07-16 08:35:08 +0200

    Fix reverse URL for item detail view in items.html - Added correct 'detail' route in item/urls.py - Updated items.html to link to item detail view properly - Small update to products/menu.html

--- item/urls.py
+++ item/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
-    path('', views.browse_items, name='browse_items'),  # Changed from 'index' to 'browse_items'
-    path('new/', views.create_item, name='create_item'),  # Changed from 'create' to 'create_item'
-    path('<int:pk>/', views.item_detail, name='item_detail'),  # Changed from 'detail' to 'item_detail'
-    path('<int:pk>/delete/', views.delete_item, name='delete_item'),  # Changed from 'delete' and function name
-    path('<int:pk>/edit/', views.edit_item, name='edit_item'),  # Changed from 'edit' and function name
+    path('', views.browse_items, name='browse_items'),
+    path('new/', views.create_item, name='create_item'),
+    path('<int:pk>/', views.item_detail, name='item_detail'),
+    path('<int:pk>/delete/', views.delete_item, name='delete_item'),
+    path('<int:pk>/edit/', views.edit_item, name='edit_item'),
 ]
