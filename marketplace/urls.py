9421f24b76370b57019285e247b1ddf1e9c7fade
LX <132520325+laminsaidy@users.noreply.github.com>
2025-07-22 17:20:40 +0200

    Refactor marketplace/urls.py imports for clarity and cleanup

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from products import views as product_views
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,7 +8,7 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('conversation/', include('conversation.urls')),
     path('items/', include('item.urls')),
-    path('protected/', views.protected_view, name='protected_view'),
+    path('protected/', product_views.protected_view, name='protected_view'),
 ]
 
 # ✅ Point to your custom 404 view
